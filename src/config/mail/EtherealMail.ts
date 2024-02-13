08cc350c1db8cc4087133c23faa7789f6e460cc9
Lucas Eduardo <lucas01.dev@gmail.com>
2024-02-13 20:59:23 -0300

    Configuração do handlebars e envio de forgotpassword example

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,13 +1,32 @@
 import nodemailer from 'nodemailer';
+import handlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ITemplateVariable {
+  [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariable;
+}
 
 interface ISendMail {
-  to: string;
-  body: string;
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
 }
 
 export default class EtherealMail {
-  static async sendMail({ to, body }: ISendMail): Promise<void> {
+  static async sendMail({ to, from, subject, templateData }: ISendMail): Promise<void> {
     const account = await nodemailer.createTestAccount();
+    const mailTemplate = new handlebarsMailTemplate();
+
     const transporter = nodemailer.createTransport({
       host: account.smtp.host,
       port: account.smtp.port,
@@ -19,10 +38,16 @@ export default class EtherealMail {
     });
 
     const message = await transporter.sendMail({
-      from: 'noreply@nodemailer.com',
-      to,
-      subject: 'Recuperação de senha',
-      text: body
+      from: {
+        name: from?.email || 'API Vendas',
+        address: from?.email || 'noreply@nodemailer.com',
+      },
+      to: {
+        name: to.email || 'API Vendas',
+        address: to.email || 'noreply@nodemailer.com',
+      },
+      subject,
+      html: await mailTemplate.parse(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
